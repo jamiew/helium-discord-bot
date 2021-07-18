a0008c1b4d0fa381f8347d4069e9de07f971a201
Casey Pugh <me@caseypugh.com>
2021-07-18 21:45:44 +0100

    update server script

--- server.sh
+++ server.sh
@@ -2,6 +2,6 @@
 # Helper script to start a permanent node server
 
 source .env
-forever stop $SERVER_UID
-forever --uid $SERVER_UID -a start bot.js
-tail -f /home/pi/.forever/loris.log
+forever stop "$SERVER_UID"
+forever --uid "$SERVER_UID" -a start bot.js
+tail -f /home/pi/.forever/${SERVER_UID}.log
